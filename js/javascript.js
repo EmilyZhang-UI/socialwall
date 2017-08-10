943353071619f90aafddd2c24949e74b2716663c
Summerqf <summerui.zh@gmail.com>
2017-08-10 13:07:43 -0400

    correct the json file

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,6 @@
 $.ajax({
   url: "https://raw.githubusercontent.com/EmilyZhang-UI/socialwall/master/data/posts.json"
 }).done(function(data){
+  data = JSON.parse(data);
   console.log(data);
-})
+});
